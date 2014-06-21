9db50bbc9695c665ce2bef62a5f4c82eec3bc06c
Olar Alca <oszckar@gmail.com>
2014-06-21 12:21:57 -0500

    [ADD] first snippet without correct namespacing

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Website Extra Snippets',
+    'description': '''
+
+    Odoo Website Extra Snippets
+
+    Here you will find some extra snippets for the website Odoo Website builder
+
+    ''',
+    'category': 'Website',
+    'version': '1.0',
+    'author': 'Vauxoo SA',
+    'depends': ['website'],
+    'data': [
+        'views/snippets.xml',
+        'views/layout.xml',
+    ],
+    'application': True,
+}
